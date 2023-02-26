67809503543a796183cd92510cba1411ff08df39
Khash <khashayar.m.003@gmail.com>
2023-02-26 17:38:52 +0330

    api and getCourses in data

---
+++ client/src/Data.js
@@ -0,0 +1,41 @@
+import apiBaseUrl from './config'
+
+export default class Data {
+    api(
+        path,
+        method= 'GET',
+        body = null,
+        requiresAuth = false,
+        credentials = null
+        ) {
+        const url = apiBaseUrl + path;
+
+        const options = {
+            method,
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+        };
+        if (body !== null) {
+            options.body = JSON.stringify(body);
+        }
+        //check if auth is required
+        if (requiresAuth) {
+            const encodedCredentials = btoa(`${credentials.usernam}:${credentials.password}`);
+
+            options.headers['Authorization'] = `Basic ${encodedCredentials}`
+        }
+        return fetch(url, options);
+    }
+
+    /**GET request to get all courses */
+    async getCourses() {
+        const res = await this.api('/courses', 'GET', null,false,null);
+
+        if (res.status === 200) {
+            return res.json().then((data) => data);
+        } else {
+            throw new Error();
+        }
+    }
+}
