b84cf6716d213d7dba9080f1c8f477140c9de732
Khash <khashayar.m.003@gmail.com>
2023-03-02 13:43:18 +0330

    made all types of error components,

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,9 @@ import UserSignIn from "./components/UserSignIn";
 import UserSignOut from "./components/UserSignOut";
 import CreateCourse from "./components/CreateCourse";
 import PrivateRoute from "./PrivateRoute";
-
+import NotFound from "./components/NotFound";
+import Forbidden from "./components/Forbidden";
+import UnhandledError from "./components/UnhandledError";
 
 const CoursesWithContext = withContext(Courses);
 const HeaderWithContext = withContext(Header);
@@ -20,6 +22,7 @@ const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
 const CreateCourseWithContext = withContext(CreateCourse);
 
+
 const App = () => {
   return (
     <Router>
@@ -28,18 +31,30 @@ const App = () => {
         {
           <Routes>
             {/*  Navigates to the course creation page - Route is private and requires authentication. */}
+
             <Route element={<PrivateRoute />}>
               <Route
                 path="/courses/create"
                 element={<CreateCourseWithContext />}
               />
+
             </Route>
 
             <Route path="/" element={<CoursesWithContext />} />
+
             <Route path="/courses/:id" element={<CourseDetailWithContext />} />
+
             <Route path="/signup" element={<UserSignUpWithContext />} />
+
             <Route path="/signin" element={<UserSignInWithContext />} />
+
             <Route path="/signout" element={<UserSignOutWithContext />} />
+
+            <Route path="*" element={<NotFound />} />
+            <Route path="/notfound" element={<NotFound/>} />
+            <Route path="/forbidden" element={<Forbidden />} />
+            <Route path="/error"  element={<UnhandledError />} />
+
           </Routes>
         }
       </main>
---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Forbidden() {
+    return(
+        <div className='wrap'>
+            <h2>Forbidden</h2>
+            <p>You don't have permission to access this page.</p>
+        </div>
+    );
+}
