c35d29c63eddfaa163a0addca9b33f3b0b6133d5
Khash <khashayar.m.003@gmail.com>
2023-07-05 20:39:37 +0430

    rohald helping :D

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 const apiBaseUrl = 'https://techdegree-fsjs-project-10-production.up.railway.app/';
-// const apiBaseUrl = 'http://localhost:7223/api';
+// const apiBaseUrl = 'http://localhost:5000/api';
 
 export default apiBaseUrl;
